7014f6fc4e2fc32291d8299461e6826d2df51a39
anhhatong <tonghaanh1999@gmail.com>
2023-04-26 16:14:57 -0400

    Project init

---
+++ src/main/kotlin/com/vacaytoday/vacaytoday/VacaytodayApplication.kt
@@ -0,0 +1,11 @@
+package com.vacaytoday.vacaytoday
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VacaytodayApplication
+
+fun main(args: Array<String>) {
+	runApplication<VacaytodayApplication>(*args)
+}
---
+++ src/test/kotlin/com/vacaytoday/vacaytoday/VacaytodayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vacaytoday.vacaytoday
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VacaytodayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
